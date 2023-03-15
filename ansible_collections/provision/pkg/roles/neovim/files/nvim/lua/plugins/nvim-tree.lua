f4e8c5714003c9090c1362fbd5a335445aba439c
saengu <saengu@home.io>
2023-03-15 07:39:19 +0700

    Update nvim-tree config to support open directory

--- ansible_collections/provision/pkg/roles/neovim/files/nvim/lua/plugins/nvim-tree.lua
+++ ansible_collections/provision/pkg/roles/neovim/files/nvim/lua/plugins/nvim-tree.lua
@@ -2,24 +2,46 @@ local M = {
   'nvim-tree/nvim-tree.lua', config=true,
 }
 
+
+-- Open nvim-tree if nvim open directory as netrw do
+local function open_nvim_tree(data)
+  -- buffer is a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
+
+  if not directory then
+    return
+  end
+
+  -- change to the directory
+  vim.cmd.cd(data.file)
+
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+
 function M.config()
-     local tree = require('nvim-tree')
-     local wk = require("which-key")
+  local tree = require('nvim-tree')
+  local wk = require("which-key")
 
-     tree.setup()
+  -- Open nvim-tree in current buffer when nvim open a directory
+  -- Refer to https://github.com/nvim-tree/nvim-tree.lua/wiki/Open-At-Startup
+  vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 
-     local mappings = {
-       e = {
-         name = "+NvimTree",
-         t = { ":NvimTreeToggle<CR>", "Toggle Tree" },
-         f = { ":NvimTreeFocus<CR>", "Open and Focus on Tree" },
-         s = { ":NvimTreeFindFile<CR>", "Find Current File" },
-       }  
-     }
-     wk.register(mappings, {
-       mode = "n", -- Normal mode
-       prefix = "<leader>"
-     })
+  tree.setup()
+
+  local mappings = {
+    e = {
+      name = "+NvimTree",
+      t = { ":NvimTreeToggle<CR>", "Toggle Tree" },
+      f = { ":NvimTreeFocus<CR>", "Open and Focus on Tree" },
+      s = { ":NvimTreeFindFile<CR>", "Find Current File" },
+    }
+  }
+
+  wk.register(mappings, {
+    mode = "n", -- Normal mode
+    prefix = "<leader>"
+  })
 end
 
 return M
