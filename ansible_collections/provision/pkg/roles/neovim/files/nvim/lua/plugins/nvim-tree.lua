b5c34c508254a660e1e34b7861cf2bc8da7708fb
saengu <saengu@home.io>
2023-03-18 17:50:13 +0700

    Auto close nvim-tree if it is last buffer

--- ansible_collections/provision/pkg/roles/neovim/files/nvim/lua/plugins/nvim-tree.lua
+++ ansible_collections/provision/pkg/roles/neovim/files/nvim/lua/plugins/nvim-tree.lua
@@ -28,21 +28,24 @@ function M.config()
   vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 
   -- Close nvim-tree if it is the last buffer
-  --vim.api.nvim_create_autocmd("BufEnter", {
-  --  nested = true,
-  --  callback = function()
-  --    if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
-  --      vim.cmd "quit"
-  --    end
-  --  end
-  --})
+  -- Refer to https://github.com/nvim-tree/nvim-tree.lua/wiki/Auto-Close#beauwilliams
+  vim.api.nvim_create_autocmd("BufEnter", {
+    group = vim.api.nvim_create_augroup("NvimTreeClose", {clear = true}),
+    pattern = "NvimTree_*",
+    callback = function()
+      local layout = vim.api.nvim_call_function("winlayout", {})
+      if layout[1] == "leaf" and vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), "filetype") == "NvimTree" and layout[3] == nil then vim.cmd("confirm quit") end
+    end
+  })
 
+  -- Show diagnostic icons
   tree.setup({
     diagnostics = {
       enable = true
     }
   })
 
+  -- Set key mappings
   local mappings = {
     e = {
       name = "+NvimTree",
