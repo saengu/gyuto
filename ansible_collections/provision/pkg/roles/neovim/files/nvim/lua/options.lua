4ce1811c0f04a590769fadfa6d409b71f1ba39d5
saengu <saengu@home.io>
2023-03-21 02:31:05 +0700

    Turn on termguicilors only on terminal app supporting 256 colors

--- ansible_collections/provision/pkg/roles/neovim/files/nvim/lua/options.lua
+++ ansible_collections/provision/pkg/roles/neovim/files/nvim/lua/options.lua
@@ -1,24 +1,27 @@
 local g = vim.g
-local opt = vim.opt
+local o = vim.opt
 
 g.mapleader = " "
 g.maplocalleader = " "
 
---opt.cmdheight = 0             -- hide command line unless needed
-opt.encoding = "utf-8"
-opt.expandtab = true          -- Enable the use of space in tab
-opt.mouse = "a"               --Enable mouse mode
-opt.number = true             --Make line numbers default
-opt.relativenumber = true     --Make relative number default
-opt.shiftwidth = 2
-opt.tabstop = 2
+--o.cmdheight = 0             -- hide command line unless needed
+o.encoding = "utf-8"
+o.expandtab = true          -- Enable the use of space in tab
+o.mouse = "a"               --Enable mouse mode
+o.number = true             --Make line numbers default
+o.relativenumber = true     --Make relative number default
+o.shiftwidth = 2
+o.tabstop = 2
+
 
 -- Since Terminal.app on MacOS only supports 256 colors, which is not the 24-bit color that truecolors uses,
--- characters will look dim. Either change to iTerm.app or disable this feature.
---opt.termguicolors = true      -- Enable colors in terminal
+-- characters will look dim. Use a guard to set termguicolors only for terminals support 256 colors.
+if os.getenv("TERM_PROGRAM") ~= 'Apple_Terminal' then
+  o.termguicolors = true      -- Enable colors in terminal
+end
 
-opt.hlsearch = true           --Set highlight on search
-opt.clipboard = "unnamedplus" -- Access system clipboard
-opt.ignorecase = true         --Case insensitive searching unless /C or capital in search
-opt.smartcase = true          -- Smart case
+o.hlsearch = true           --Set highlight on search
+o.clipboard = "unnamedplus" -- Access system clipboard
+o.ignorecase = true         --Case insensitive searching unless /C or capital in search
+o.smartcase = true          -- Smart case
 
